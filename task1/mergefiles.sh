5fbf21f5a7417ed9b3badaa210f9d91a145490fe
Skandan Ramesh <54405114+skandyman@users.noreply.github.com>
2021-08-24 22:49:35 +0530

    Update mergefiles.sh

--- task1/mergefiles.sh
+++ task1/mergefiles.sh
@@ -7,7 +7,7 @@ do
 	file2existence=$?
 	if [ $file2existence == 0 ]
 	then
-		salaries="$(awk -F ',' -v row=$file2line -v col=2 'NR==row{print $col}' ./file2.txt)"
+		salaries="$(awk -F ',' -v row="$file2line" -v col=2 'NR==row{print $col}' ./file2.txt)"
 	else
 		salaries="N/A"
 	fi
@@ -16,8 +16,8 @@ do
 	file3existence=$?
 	if [ $file3existence == 0 ]
 	then
-		email="$(awk -F ',' -v row=$file3line -v col=1 'NR==row{print $col}' ./file3.txt)"
-		year="$(awk -F ',' -v row=$file3line -v col=3 'NR==row{print $col}' ./file3.txt)"
+		email="$(awk -F ',' -v row="$file3line" -v col=1 'NR==row{print $col}' ./file3.txt)"
+		year="$(awk -F ',' -v row="$file3line" -v col=3 'NR==row{print $col}' ./file3.txt)"
 	else
 		email="N/A"
 		year="N/A"
